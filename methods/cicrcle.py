0baa2874433341c971739777ddc629dd25ebe15b
Kostya <beztalannyjkonstantin@gmail.com>
2025-11-08 09:36:00 +0000

    easy

---
+++ methods/cicrcle.py
@@ -0,0 +1,14 @@
+from math import pi
+
+
+class Circle:
+    def __init__(self, radius: int | float):
+        self.radius = radius
+        self.diameter = 2 * self.radius
+        self.area = pi * self.radius**2
+
+
+circle = Circle(5)
+print(circle.radius)
+print(circle.diameter)
+print(circle.area)

57fda4cba02392ea24945fd00421223dbfef3692
Kostya <beztalannyjkonstantin@gmail.com>
2025-11-11 10:29:25 +0000

    the first task with magic methods

---
+++ magic/config.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from typing import Any
+
+
+class Config:
+    _instance = None
+
+    def __new__(cls, *args: Any, **kwargs: Any) -> Config:
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self) -> None:
+        self.program_name = "GenerationPy"
+        self.environment = "release"
+        self.loglevel = "verbose"
+        self.version = "1.0.0"

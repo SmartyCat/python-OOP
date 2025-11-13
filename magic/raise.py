81941bfac8fb78735af15e16e6ba5d8f748e7ee7
Kostya <beztalannyjkonstantin@gmail.com>
2025-11-13 13:39:12 +0000

    really easy task

---
+++ magic/raise.py
@@ -0,0 +1,11 @@
+class RaiseTo:
+    def __init__(self, degree: int) -> None:
+        self.degree = degree
+
+    def __call__(self, x: int) -> int:
+        return x**self.degree
+
+raise_to_three = RaiseTo(3)
+raise_to_four = RaiseTo(4)
+print(raise_to_three(3))
+print(raise_to_four(2))

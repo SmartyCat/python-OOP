8738838bacebee8d57f5168f05b865eacc7980c7
Kostya <beztalannyjkonstantin@gmail.com>
2025-11-13 16:04:22 +0000

    task with date

---
+++ magic/date_formatter.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+
+class DateFormatter:
+    d = {
+        "ru": "%d.%m.%Y",
+        "us": "%m-%d-%Y",
+        "ca": "%Y-%m-%d",
+        "br": "%d/%m/%Y",
+        "pt": "%d-%m-%Y",
+        "fr": "%d.%m.%Y",
+    }
+
+    def __init__(self, country_code: str) -> None:
+        self.__country_code = country_code
+
+    def __call__(self, d: date) -> str:
+        return d.strftime(DateFormatter.d[self.__country_code])
+
+
+ca_format = DateFormatter("ca")
+print(ca_format(date(2022, 11, 7)))

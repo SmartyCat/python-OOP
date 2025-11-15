2e54e03301b8709681a693d9928f93735333b633
Kostya <beztalannyjkonstantin@gmail.com>
2025-11-15 09:04:15 +0000

    the first task today

--- magic/attrsnumberobject.py
+++ magic/attrsnumberobject.py
@@ -4,7 +4,7 @@ from typing import Any
 class AttrsNumberObject:
     def __init__(self, **kwargs: Any) -> None:
         for k in kwargs:
-            setattr(self, k, kwargs)
+            setattr(self, k, kwargs[k])
 
     def __getattr__(self, value: str) -> str:
         return len(self.__dict__) + 1 if value == "attrs_num" else None

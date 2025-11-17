a8ca0c6ac65ebfc6d064f95b1fc7bf1417b55f09
Kostya <beztalannyjkonstantin@gmail.com>
2025-11-17 11:56:21 +0000

    the third task with iter class

---
+++ protol/attrsiterator.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from typing import Any
+
+
+class AttrsIterator:
+    def __init__(self, obj: Any) -> None:
+        self.__obj = tuple(obj.__dict__.items())
+        self.__index = 0
+
+    def __iter__(self) -> AttrsIterator:
+        return self
+
+    def __next__(self) -> Any:
+        if self.__index == len(self.__obj):
+            raise StopIteration
+        self.__index += 1
+        return self.__obj[self.__index - 1]

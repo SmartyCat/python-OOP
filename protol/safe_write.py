8cf99606224d240282c505e694feafc497702c9e
Kostya <beztalannyjkonstantin@gmail.com>
2025-11-24 16:06:45 +0000

    i am so tired

--- protol/safe_write.py
+++ protol/safe_write.py
@@ -1,27 +1,23 @@
 from typing import Generator
 from contextlib import contextmanager
 
-string_check = None
+data = []
 
 
 @contextmanager
 def safe_write(filename: str) -> Generator:
     try:
-        with open(filename, "a", encoding="utf-8") as file:
+        with open(filename, "w", encoding="utf-8") as file:
             yield file
-        with open(filename, "r+", encoding="utf-8") as file:
-            global string_check
-            string_check = len(file.readlines())
+        with open(filename, encoding="utf-8") as file:
+            data.append(file.readlines())
     except Exception as error:
-        with open(filename, "r", encoding="utf-8") as file:
-            strings = file.readlines()
-            with open(filename, "w", encoding="utf-8") as output:
-                if string_check != len(strings):
-                    strings[-2] = strings[-2].strip()
-                    output.writelines(strings[:-1])
-                else:
-                    strings[-1] = strings[-1].strip()
-                    output.writelines(strings)
         print(
             f"Во время записи в файл было возбуждено исключение {type(error).__name__}"
         )
+
+    finally:
+        with open(filename, "w", encoding="utf-8") as file:
+            result = data[0]
+            result[-1] = result[-1].strip()
+            file.writelines(result)

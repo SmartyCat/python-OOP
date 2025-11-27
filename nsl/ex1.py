c8df2263fa78f1934dcf2241a003b02e09613e36
Kostya <beztalannyjkonstantin@gmail.com>
2025-11-27 10:34:25 +0000

    the first task today

---
+++ nsl/ex1.py
@@ -0,0 +1,30 @@
+class Vehicle:
+    pass
+
+
+class LandVehicle(Vehicle):
+    pass
+
+
+class WaterVehicle(Vehicle):
+    pass
+
+
+class AirVehicle(Vehicle):
+    pass
+
+
+class Car(LandVehicle):
+    pass
+
+
+class Motorcycle(LandVehicle):
+    pass
+
+
+class Popeller(AirVehicle):
+    pass
+
+
+class Jet(AirVehicle):
+    pass
